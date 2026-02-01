d3faa327bfc5099d9a0eeee77480e0a7b918c40a
José Vítor Pinto <jose.v.pinto8@aluno.senai.br>
2026-02-01 02:48:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -73,9 +73,6 @@ function renderProducts() {
             <p class="price-installment">ou ${installments}x de R$ ${fmt(installmentValue)} sem juros</p>
           </div>
           <button class="add-to-cart-btn" data-id="${id}">
-            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-            </svg>
             Adicionar
           </button>
         </div>
